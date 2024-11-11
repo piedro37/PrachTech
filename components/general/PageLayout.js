30dcb985468fcd22c8a6842a9beeed05aa362dd4
Pieter Van Eynde <pieter.vaneynde@vrt.be>
2024-11-11 17:14:27 +0100

    vat page

--- components/general/PageLayout.js
+++ components/general/PageLayout.js
@@ -1,85 +1,81 @@
-
-import styled, { ThemeProvider } from 'styled-components'
-import Footer from './Footer'
-import Head from 'next/head'
-import Navbar from '../navbar/Navbar'
-import { darkTheme, lightTheme } from '../../styles/themes'
-import { useSelector } from 'react-redux'
+import Head from 'next/head';
+import { useSelector } from 'react-redux';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from '../../styles/themes';
+import Navbar from '../navbar/Navbar';
+import Footer from './Footer';
 
 const Container = styled.div`
-    min-height: 100vh;
-    padding: 1rem 0.5rem 0 0.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: ${props => props.theme.background};
-    color: ${props => props.theme.primary};
+  min-height: 100vh;
+  padding: 1rem 0.5rem 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.primary};
 
-    h1 {
-        color: ${props => props.theme.h1};
-        line-height: 1.15;
-        font-size: 2.8rem;
-        margin: 3rem 0 0.5rem 0;
-    }
-    
-    h2 {
-        color: ${props => props.theme.h2};
-        font-size: 1.5rem;
-        /* margin: 0.5rem 0; */
-    }
+  h1 {
+    color: ${(props) => props.theme.h1};
+    line-height: 1.15;
+    font-size: 2.8rem;
+    margin: 3rem 0 0.5rem 0;
+  }
 
-    h3 {
-        color: ${props => props.theme.h3};
-        margin: 1rem 0;
-    }
+  h2 {
+    color: ${(props) => props.theme.h2};
+    font-size: 1.5rem;
+    /* margin: 0.5rem 0; */
+  }
 
-    p {
-        color: ${props => props.theme.p};
-        line-height: 1.5;
-        /* margin: 0.5rem 0; */
-    }
-    
-    a:hover {
-        color: ${props => props.theme.hover};
-    }
-`
+  h3 {
+    color: ${(props) => props.theme.h3};
+    margin: 1rem 0;
+  }
+
+  p {
+    color: ${(props) => props.theme.p};
+    line-height: 1.5;
+    /* margin: 0.5rem 0; */
+  }
+
+  a:hover {
+    color: ${(props) => props.theme.hover};
+  }
+`;
 
 const Main = styled.div`
-    padding: 5rem 1rem;
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    max-width: 800px;
-`
+  padding: 5rem 1rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 800px;
+`;
 
 /** Layout used for all pages */
 const PageLayout = ({ children, title }) => {
-    const dark = useSelector(state => state.general.darkTheme)
+  const dark = useSelector((state) => state.general.darkTheme);
 
+  return (
+    <ThemeProvider theme={dark ? darkTheme : lightTheme}>
+      <Container>
+        <Head>
+          <title>{title}</title>
+          <link rel='icon' href='/favicon.ico' />
+          <meta
+            name='description'
+            content='PrachTech - Pieter Van Eynde - Freelance React Developer.'
+          />
+        </Head>
+        <Navbar />
 
-    return (
-        <ThemeProvider theme={dark ? darkTheme : lightTheme}>
-            <Container>
-                <Head>
-                    <title>{title}</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <meta
-                        name="description"
-                        content="PrachTech - Pieter Van Eynde - Freelance React Developer."
-                    />
-                </Head>
-                <Navbar />
+        <Main>{children}</Main>
+        <Footer />
+      </Container>
+    </ThemeProvider>
+  );
+};
 
-                <Main>
-                    {children}
-                </Main>
-                <Footer />
-            </Container>
-        </ThemeProvider>
-    )
-}
-
-export default PageLayout
+export default PageLayout;
---
+++ pages/vat/index.js
@@ -0,0 +1,13 @@
+import PageLayout from '../../components/general/PageLayout';
+
+export default function Vat() {
+  return (
+    <PageLayout title='PrachTech BV'>
+      <div>
+        <h2>PrachTech BV</h2>
+        <p>BE 0727 479 313</p>
+        <p>Lepelhoekstraat 83A, 9100 Sint-Niklaas</p>
+      </div>
+    </PageLayout>
+  );
+}
