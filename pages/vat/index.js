9ec4b04baab399e7cd9db1f63646060fd26e9930
Pieter Van Eynde <pieter.vaneynde@vrt.be>
2024-11-19 10:18:08 +0100

    add email

--- pages/vat/index.js
+++ pages/vat/index.js
@@ -8,6 +8,7 @@ export default function Vat() {
         <h2>BE 0727 479 313</h2>
         <p>Lepelhoekstraat 83A</p>
         <p>9100 Sint-Niklaas</p>
+        <p>pieter@prach.tech</p>
       </div>
     </PageLayout>
   );
